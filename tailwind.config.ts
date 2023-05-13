9a80d9cc1fea39c9d4df9530ef20b9162e34e933
Eduardo Rivas <eduardoeerivas299@gmail.com>
2023-05-13 09:07:52 -0500

    added stuff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./node_modules/flowbite-react/**/*.js",
+  "./pages/**/*.{ts,tsx}",
+  "./public/**/*.html",
+],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 } satisfies Config;
